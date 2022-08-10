82d1d2be12110e799e9c9a8768327f237a7e5b9f
Ron van Hezik <ron@hezik.nl>
2022-08-10 11:38:31 +0000

    tailwind-daisyui add, devsetting-extension changed

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+    require('daisyui'),
+    'tailwindcss',
+    'autoprefixer',
+  ],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: '',
+    darkTheme: 'dark',
+  },
+};
