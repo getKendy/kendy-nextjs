2a94779788997ee85fee825ff7824342feb0ee34
Ron van Hezik <ron@hezik.nl>
2022-08-30 11:01:25 +0200

    test

--- pages/statics.js
+++ pages/statics.js
@@ -57,7 +57,7 @@ export async function getServerSideProps(context) {
   if (!session) {
     return {
       redirect: {
-        destination: '/login',
+        destination: '/api/auth/signin',
         permanent: false,
       },
     }
