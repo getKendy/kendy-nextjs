326521d4314d4cf7e13a3d92c0cc6fc0d3f1ac91
Ron van Hezik <ron@hezik.nl>
2023-01-17 18:41:12 +0100

    baas

---
+++ components/NewsCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function NewsCard({ article }) {
+  return (
+    <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+      <div className="px-6 py-4">
+        <h3 className="text-lg font-medium text-gray-900">{article.title}</h3>
+        <p className="text-sm text-gray-600">{article.description}</p>
+        <a href={article.url} className="text-blue-500">
+          Read More
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default NewsCard;
